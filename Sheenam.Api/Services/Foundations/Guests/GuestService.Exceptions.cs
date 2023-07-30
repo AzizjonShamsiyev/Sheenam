4b65466350ce36e58cdb4b2a00f19a1075d2bdf4
Azizjon Shamsiyev <azizjon5629@gmail.com>
2023-07-30 04:23:14 -0700

    ShouldShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.Exceptions.cs
@@ -3,6 +3,7 @@
 //Free To Use To Find Comfort and Peace
 //============================================
 
+using Microsoft.Data.SqlClient;
 using Sheenam.Api.Models.Foundations.Guests;
 using Sheenam.Api.Services.Foundations.Guests.Exceptions;
 using System.Threading.Tasks;
@@ -28,6 +29,12 @@ namespace Sheenam.Api.Services.Foundations.Guests
             {
                 throw CreateAndLogValidationException(invalidGuestException);
             }
+            catch(SqlException sqlException)
+            {
+                var failedGuestStorageException = new FailedGuestStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedGuestStorageException);
+            }
         }
          
         private GuestValidationException CreateAndLogValidationException(Xeption exception)
@@ -38,6 +45,16 @@ namespace Sheenam.Api.Services.Foundations.Guests
             this.loggingBroker.LogError(guestValidationException);
 
             return guestValidationException;
+        }
+
+        private GuestDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var guestDependencyException =
+                new GuestDependencyException(exception);
+
+            this.loggingBroker.LogCritical(guestDependencyException);
+
+            return guestDependencyException;
         }
     }
 }
